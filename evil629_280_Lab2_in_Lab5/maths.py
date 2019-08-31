4d83c8dad3221d460852ce24fb7f783e64521525
III ECL1PS3 III <43918644+EnzoVillarama@users.noreply.github.com>
2019-08-31 20:16:11 +1200

    Ugh, problems testing merging

--- evil629_280_Lab2_in_Lab5/maths.py
+++ evil629_280_Lab2_in_Lab5/maths.py
@@ -58,14 +58,6 @@ def convert_base(num, n):
 
 
 def factorial(number):
-    
-    if number == 0:
-        return number
-    
-    else:
-        return number * factorial(number - 1)
-        
-
 
     
     
