1229c2b349a193893d68cae1236043b9b5966114
III ECL1PS3 III <43918644+EnzoVillarama@users.noreply.github.com>
2019-08-31 20:19:51 +1200

    Factorials!

--- evil629_280_Lab2_in_Lab5/maths.py
+++ evil629_280_Lab2_in_Lab5/maths.py
@@ -58,15 +58,9 @@ def convert_base(num, n):
 
 
 def factorial(number):
-<<<<<<< HEAD
-    
+
    if number == 0:
        return number
    
    else:
        return number * factorial(number - 1)
-=======
-
-    
-    
->>>>>>> refs/heads/master
